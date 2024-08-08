7c256f560c7002e43517b1b06bede058ee961a1a
borxy12 sada <sinurmasrina10@gmail.com>
2024-08-08 13:03:17 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xad9dc4C97c5f6654E72e668aec80696d34f998C9'
+export const ProxyAdmin = '0x4D10E1D263fe0d809FC5142CA9b1e75513CfAFA5'
+export const SWTRImplementation = '0x9ef96ACEcd500D030C0801BD2B38474917705646'
     
